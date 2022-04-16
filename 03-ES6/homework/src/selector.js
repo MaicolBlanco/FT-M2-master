a805e62945b4b3d167375f11259634346202afbb
MaicolBlanco <maicolblancoalvarez@gmail.com>
2022-04-16 17:43:48 -0500

    Solution homework about Ecmascript6, search to ids, tags, class, etc

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -9,7 +9,13 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
-  
+  if(matchFunc(startEl)) resultSet.push(startEl)
+
+  for (let i = 0; i < startEl.children.length; i++) {
+    var elementos = traverseDomAndCollectElements(matchFunc, startEl.children[i])
+    resultSet = resultSet.concat(elementos)
+  }
+  return resultSet
 };
 
 // Detecta y devuelve el tipo de selector
@@ -18,6 +24,13 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
+  // if(selector[0] === '.') return 'class'
+  // else if(selector[0] === '#') return 'id'
+  // // else if(selector[3] === '.') return 'tag.class'
+  // else if(selector.split('.').length > 1) return 'tag.class'
+  // else return 'tag'
+  /* Ecmascript 6 */
+  return selector[0] === '#' ? 'id' : selector[0] === '.' ? 'class' : selector.split('.').length > 1 ? 'tag.class' : 'tag'
   
 };
 
@@ -30,13 +43,28 @@ var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") { 
-   
+    // matchFunction = function(elemento){
+    //   if('#' + elemento.id === selector) return true
+    //   else return false
+    // }
+    /* Ecmascript 6 */
+    matchFunction = (elemento) => `#${elemento.id}` === selector ? true : false
   } else if (selectorType === "class") {
-    
+    matchFunction = (elemento) => {
+      var clases = elemento.classList
+      for (let i = 0; i < clases.length; i++) {
+        if(`.${clases[i]}` === selector) return true
+      }return false
+    }
   } else if (selectorType === "tag.class") {
-    
+    matchFunction = (elemento) => {
+      // selector.split('.')[0]
+      // selector.split('.')[1]
+      let [tag, clase] = selector.split('.')
+      return matchFunctionMaker(tag)(elemento) && matchFunctionMaker(`.${clase}`)(elemento)
+    }
   } else if (selectorType === "tag") {
-    
+    matchFunction = (elemento) => elemento.tagName.toLowerCase() === selector
   }
   return matchFunction;
 };
